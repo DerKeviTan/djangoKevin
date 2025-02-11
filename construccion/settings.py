d291ea5843f14d63e6f1a320ddf89135b03885b5
DerKeviTan <154334806+DerKeviTan@users.noreply.github.com>
2025-02-11 11:54:54 -0500

    Update settings.py

--- construccion/settings.py
+++ construccion/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'Aplicaciones.viales',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'construccion.urls'
